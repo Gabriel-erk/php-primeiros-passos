9dbb30f011ba7dae5de2724496bd90fd4829ff1c
Gabriel Erick <gabrielmmve13@gmail.com>
2025-12-22 20:16:02 -0300

    exercicio com array associativo/dicionario

--- php-criando-sua-aplicacao/exercicios-topico-arrays-em-php/ex004.php
+++ php-criando-sua-aplicacao/exercicios-topico-arrays-em-php/ex004.php
@@ -1,4 +1,41 @@
-<?php 
+<?php
 
+$familiares = [
+    "familiaParteDeMae" => [
+        "tia" => [
+            "nome" => "leticia",
+            "idade" => 32,
+        ],
+        "tio" => [
+            "nome" => "christiano",
+            "idade" => 42,
+        ],
+        "primo" => [
+            "nome" => "kaleb",
+            "idade" => 1,
+        ],
+    ],
+    "familiaParteDePai" => [
+        "tia" => [
+            "nome" => "silvia",
+            "idade" => 52,
+        ],
+        "tio" => [
+            "nome" => "carlos",
+            "idade" => 55,
+        ],
+        "primo" => [
+            "nome" => "roberto",
+            "idade" => 22,
+        ],
+    ],
+];
 
+$jose = [
+    "nome" => "jose",
+    "idade" => 30,
+];
 
+$familiares["familiaParteDePai"]["tioDistante"] = $jose;
+
+var_dump($familiares);
