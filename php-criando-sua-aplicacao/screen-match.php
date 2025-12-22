e7beadab3e1d97e3cc1367b9f94cd14683594c16
Gabriel Erick <gabrielmmve13@gmail.com>
2025-12-22 17:26:29 -0300

    aprimorações do código

--- php-criando-sua-aplicacao/screen-match.php
+++ php-criando-sua-aplicacao/screen-match.php
@@ -1,10 +1,32 @@
 <?php
 
 echo "Bem-vindo(a) ao Screen Match!" . PHP_EOL;
-$filmes = ["Se beber, não case!", "Thor: Ragnarok", "Top Gun - Maverick"];
+$escolhaDoUsuario = (int) readline("Digite o número do filme (0 a 2): ");
+
+while ($escolhaDoUsuario < 0 || $escolhaDoUsuario > 2) {
+    echo "Número inválido. Digite novamente." . PHP_EOL;
+    $escolhaDoUsuario = (int) readline("Digite o número do filme (0 a 2): ");
+}
+// $filmes = ["Se beber, não case!", "Thor: Ragnarok", "Top Gun - Maverick"];
+
+$filmes =
+    [
+        [
+            "nome" => "Se beber, não case!",
+            "anoLancamento" => 2011,
+        ],
+        [
+            "nome" => "Thor: Ragnarok",
+            "anoLancamento" => 2017,
+        ],
+        [
+            "nome" => "Top Gun - Maverick",
+            "anoLancamento" => 2022,
+        ]
+    ];
 
 // argv é um array especial do php que guarda os argumentos passados via linha de comando (TERMINAL), separados por espaço, ou seja, ao digitar "php screen-match.php 2022", o valor 2022 estará em $argv[1] e o valor $argv[0] será o nome do arquivo "screen-match.php"
-$anoLancamento = 2022; // se não for passado nenhum argumento (ou seja, se for nulo), o ano de lançamento será 2022
+$anoLancamento = $filmes[$escolhaDoUsuario]["anoLancamento"]; // se não for passado nenhum argumento (ou seja, se for nulo), o ano de lançamento será 2022 ex: $filmes[$escolhaDoUsuario]["anoLancamento"] ?? 2022;
 
 $quantidadeDeNotas = $argc - 1; // o valor de argc é a quantidade de argumentos passados via linha de comando (TERMINAL), incluindo o nome do arquivo, por isso subtraímos 1 para obter a quantidade real de notas (para não incluir o nome do arquivo na contagem)
 $notas = [];
@@ -21,8 +43,8 @@ $planoPrime = true;
 // se uma das condições forem verdadeiras, a váriavel $incluidoNoPlano será verdadeira (true), caso contrário, será falsa (false)
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
-echo $filmes[0] . PHP_EOL;
-echo "Lançado em: ($anoLancamento) - Nota: $notaFilme" . PHP_EOL;
+echo $filmes[$escolhaDoUsuario]["nome"] . PHP_EOL;
+echo "Lançado em: " . $anoLancamento . " - Nota: $notaFilme" . PHP_EOL;
 
 if ($anoLancamento > 2022) {
     echo "Esse filme é um lançamento.";
@@ -33,7 +55,7 @@ if ($anoLancamento > 2022) {
 }
 
 // match realiza uma comparação estrita (===) entre o valor da variável e os valores definidos em cada case, caso encontre um valor correspondente, ou seja, caso o valor passado como parâemetro seja igual a um dos valores definidos, o bloco de código correspondente será executado(se $nomeFilme passado nos parênteses de match for igual a um dos cases dele, como por exemplo, um dos cases dentro do bloco de código do nosso match é "Se beber, não case!" e se o valor de $nomeFilme for igual a "Se beber, não case!" ele vai retornar o valor "comédia" para a váriavel $genero), retornando o valor associado a esse case. Se nenhum valor corresponder, o bloco default será executado, retornando o valor associado a ele.
-$genero =  match ($filmes[0]) {
+$genero =  match ($filmes[$escolhaDoUsuario]["nome"]) {
     "Se beber, não case!" => "Comédia",
     "Thor: Ragnarok" => "super-herói",
     "Top Gun - Maverick" => "Ação",
