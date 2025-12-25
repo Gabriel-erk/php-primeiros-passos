05a038cd9f1582de48d733fc8cabea6407892963
Gabriel Erick <gabrielmmve13@gmail.com>
2025-12-25 12:19:46 -0300

    funções/retornos + tipos de retorno/parâmetros

--- php-criando-sua-aplicacao/screen-match.php
+++ php-criando-sua-aplicacao/screen-match.php
@@ -1,6 +1,6 @@
 <?php
-
-function exibeMensagemLancamento($ano)
+// subrotina que não retorna nada, apenas exibe uma mensagem na tela, por isso o tipo de retorno é void (vazio) que significa a ausência de retorno de qualquer tipo
+function exibeMensagemLancamento(int $ano): void
 {
     if ($ano > 2022) {
         echo "Esse filme é um lançamento.";
@@ -9,6 +9,15 @@ function exibeMensagemLancamento($ano)
     } else {
         echo "Esse filme não é um lançamento." . PHP_EOL;
     }
+}
+
+// definindo a função incluidoNoPlano que recebe dois parâmetros: um booleano $planoPrime e um inteiro $anoLancamento para facilitar o tratamento da lógica de inclusão no plano e não permitir que valores diferentes desses tipos sejam passados para a função, caso passe, o php irá avisar um erro de tipo de váriavel (ou em alguns casos, ele pode tentar converter o valor passado para o tipo esperado, mas isso não é garantido)
+// fazendo isso adicionamos robustez ao código, protegendo contra erros humanos, pois eu, ou alguém que esteja mexendo no código, pode acabar passando um valor errado para a função, como uma string ou um array, o que poderia causar comportamentos inesperados ou erros em tempo de execução
+// para melhorar mais ainda, podemos definir o tipo de retorno da função como booleano, utilizando ": bool" após os parênteses dos parâmetros, assim garantimos que a função sempre retornará um valor booleano (true ou false), caso contrário, o PHP irá avisar um erro de tipo de retorno
+function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool
+{
+    // return quer dizer: devolva o resultado dessa expressão para quem te chamou, lá abaixo eu chamo essa função na váriavel $incluido no plano, e ela irá me retornar o resultado da operação lógica utilizando os valores que eu passar como argumentos para a função
+    return $planoPrime || $anoLancamento < 2020;
 }
 
 echo "Bem-vindo(a) ao Screen Match!" . PHP_EOL;
@@ -52,7 +61,7 @@ $notaFilme = array_sum($notas) / $quantidadeDeNotas;
 
 $planoPrime = true;
 // se uma das condições forem verdadeiras, a váriavel $incluidoNoPlano será verdadeira (true), caso contrário, será falsa (false)
-$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
 
 echo $filmes[$escolhaDoUsuario]["nome"] . PHP_EOL;
 echo "Lançado em: " . $anoLancamento . " - Nota: $notaFilme" . PHP_EOL;
