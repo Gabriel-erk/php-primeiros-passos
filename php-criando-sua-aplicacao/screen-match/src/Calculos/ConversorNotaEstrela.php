023668858b2cdd14fc49e8c75927e9dfb0a7699b
Gabriel Erick <gabrielmmve13@gmail.com>
2026-01-19 19:51:10 -0300

    modelando e iniciando aprendizado de interface

--- php-criando-sua-aplicacao/screen-match/src/Calculos/ConversorNotaEstrela.php
+++ php-criando-sua-aplicacao/screen-match/src/Calculos/ConversorNotaEstrela.php
@@ -2,9 +2,11 @@
 
 class ConversorNotaEstrela
 {
-    public function converte(Titulo $titulo): float
+    // porque trocar o tipo de parâmetro de Titulo para Avaliavel? pois TUDO que for avaliavel vai ter o método media, incluindo minha classe episodio, que implementa a interface avaliavel (tudo que implementar a interface avaliavel irá se encaixar aqui, poderá ser passado como parâmetro para meu método "converte") agora, se eu definir como "titulo", não posso utilizar este método para meus episódios
+    public function converte(Avaliavel $avaliavel): float
     {
-        $nota = $titulo->media();
-        return 10.4;
+        $nota = $avaliavel->media();
+        // usando como parâmetro que iremos receber notas de 0 a 10 e queremos converter para 0 a 5 estrelas, arredondando para o inteiro mais próximo e depois dividindo por 2 para que fique na escala de 0 a 5 (ou seja, 0 a 5 estrelas)
+        return round($nota) / 2;
     }
 }
---
+++ php-criando-sua-aplicacao/screen-match/src/Model/Avaliavel.php
@@ -0,0 +1,12 @@
+<?php
+
+// interface é como se fosse uma classe abstrata sem propriedades onde todos os métodos são abstratos - logo, é possível implementar várias interfaces por conta de seus métodos não terem uma implementação fixa, no nosso sistema isso se aplica da seguinte forma: minha classe episódio tem algumas coisas em comum com minha classe título, porém não são iguais, existem atributos/metodos que não seriam utilziados (deixando nosso código bagunçado caso colocassemos que a classe episodio herda da classe titulo), fora que, conceitualmente (na regra de negócio do nosso sistema) um episódio não é um titulo, eles não são iguais e um um episodio não pode simplesmente existir solto (como um filme e uma série), ele depende de uma série, está vinculado a ela, porém ele também NÃO É uma série (logo, não faz sentido ele extender da mesma), por isso para criarmos ess vínculo entre série e filme utilizamos uma "chave estrangeira" na classe Episódio, onde vinculamos cada objeto instanciado a partir da classe episódio a uma série
+// a interface vem para permitir que as classes instanciadas a partir de titulo (a classe titulo não tem obrigação de preenhcer os métodos abstratos da noss interface pois ele também é abstrato, apenas por isso) tenham os métodos avalia() e media() e também permitir que a classe episodio tenha os métodos avalia() e media(), assim evitando reutilização inútil de código e deixando algo uniforme (caso episódio fosse extendido por alguma classe, a mesma teria de implementar esses 2 métodos abstratos também) 
+// uma interface é um contrato, um compromisso que eu firmo dizendo: se eu implementar essa interface (ou seja, se minha classe implementar alguma interface) eu GARANTO/EU TENHO QUE TER aqui (na classe que esta implementando a interface) todos os métodos que minha interface diz ter (afinal, uma interface não possui atributos)
+// interface, assim como uma classe também é um tipo, que pode ser utilizado como parâmetro (de funções e métodos), como retorno (de funções e métodos)
+interface Avaliavel
+{
+
+    public function avalia(): void;
+    public function media(): float;
+}
